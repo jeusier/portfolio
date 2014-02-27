b98f7aa9ca998eb375861eeb5e7fd2861939b556
jahsan87 <jahsan87@gmail.com>
2014-02-26 18:55:52 -1000

    added blog controller and model

---
+++ api/controllers/ProjectController.js
@@ -0,0 +1,81 @@
+/**
+ * ProjectController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+  
+  /**
+   * Action blueprints:
+   *    `/project/index`
+   *    `/project`
+   */
+   index: function (req, res) {
+
+    return res.view();
+
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/project/show`
+   */
+   show: function (req, res) {
+    
+    // Send a JSON response
+    return res.json({
+      hello: 'jCurray'
+    });
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/project/edit`
+   */
+   edit: function (req, res) {
+    
+    // Send a JSON response
+    return res.json({
+      hello: 'world'
+    });
+  },
+
+
+  /**
+   * Action blueprints:
+   *    `/project/delete`
+   */
+   delete: function (req, res) {
+    
+    // Send a JSON response
+    return res.json({
+      hello: 'world'
+    });
+  },
+
+
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to ProjectController)
+   */
+  _config: {}
+
+  
+};
